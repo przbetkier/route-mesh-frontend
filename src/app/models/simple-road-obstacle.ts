52f16c39d68cf3a2614ef30838ef9d5a4e9f1112
Przemyslaw Betkier <przemyslaw.betkier@allegro.pl>
2020-03-22 17:43:28 +0100

    Obstacle subtype

--- src/app/models/simple-road-obstacle.ts
+++ src/app/models/simple-road-obstacle.ts
@@ -3,7 +3,8 @@ export class SimpleRoadObstacle {
               public name: string,
               public city: string,
               public comment: string,
-              public obstacleType: string,
+              public type: string,
+              public subtype: string,
               public immovable: boolean) {
   }
 }
